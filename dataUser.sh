5d6c74910ea74cece3c44240d2dc45b9422a0a99
GumblarZ <jc0d3001@outlook.com>
2020-12-01 10:46:39 -0300

    Update dataUser.sh

--- dataUser.sh
+++ dataUser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INFOUSER=($DISPLAY $HISTFILE $HISTFILESIZE $HOME $LOGNAME $PATH $PS1 $PWD $OLDPWD)
-INFO=(':' 'Onde esta sendo salvo historico: ' 'Tamanho do historico: ' 'Home do usuario: ' 'Usuario logado: ' 'gg: ' 'ff:' 'Localizacao atual: ' 'Ultima localizacao: ') 
+INFO=(':' 'Onde esta sendo salvo historico: $HISTFILE' 'Tamanho do historico: $HISTFILESIZE' 'Home do usuario: $HOME' 'Usuario logado: $LOGNAME' 'gg: ' 'ff:' 'Localizacao atual: $PWD' 'Ultima localizacao: $OLDPWD') 
 i=0
 	while [ $i -le 8 ]
 	do 
