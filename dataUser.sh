f37c926989a58e0efc0ce1b070326bebffa950bf
gumblarz <jc0d3001@outlook.com>
2020-12-01 11:00:51 -0300

    resolvido bugs

--- dataUser.sh
+++ dataUser.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-INFO=('DISPLAY: $DISPLAY' 'Onde esta sendo salvo historico: $HISTFILE' 'Tamanho do historico: $HISTFILESIZE' 'Home do usuario: $HOME' 'Usuario logado: $LOGNAME' 'gg: ' 'ff:' 'Localizacao atual: $PWD' 'Ultima localizacao: $OLDPWD') 
+INFO=('DISPLAY:'$DISPLAY 'Onde esta sendo salvo historico: '$HISTFILE 'Tamanho do historico: '$HISTFILESIZE 'Home do usuario: '$HOME 'Usuario logado: '$LOGNAME 'Path: '$PATH 'PS1: '$PS1 'Localizacao atual: '$PWD 'Ultima localizacao: '$OLDPWD) 
 i=0
 	while [ $i -le 8 ]
 	do 
-		echo ${INFOUSER[$i]}
+		echo ${INFO[$i]}
 		i=$(( $i + 1 ))
 	done
